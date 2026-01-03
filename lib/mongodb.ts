e720602e47a843f8aadf1cea4aa1160f71aa23d9
spacepotatoes <gtroiano77@gmx.de>
2026-01-03 20:11:46 +0000

    Coderabitt change mongoose.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -9,17 +9,17 @@ type MongooseCache = {
 // Extend the global object to include our mongoose cache
 declare global {
   // eslint-disable-next-line no-var
-  var mongoose: MongooseCache | undefined;
+  var mongooseCache: MongooseCache | undefined;
 }
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 
 // Initialize the cache on the global object to persist across hot reloads in development
-let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+let cached: MongooseCache = global.mongooseCache || { conn: null, promise: null };
 
-if (!global.mongoose) {
-  global.mongoose = cached;
+if (!global.mongooseCache) {
+  global.mongooseCache = cached;
 }
 
 /**
