83d501cddf3d1b17cd5c1ce0b278deac89367edc
spacepotatoes <gtroiano77@gmx.de>
2026-01-03 20:09:31 +0000

    Coderabitt change of database/index.ts

--- database/index.ts
+++ database/index.ts
@@ -1,2 +1,7 @@
-export { Event, type EventDocument } from './event.model'
-export { Booking, type BookingDocument } from './booking.model'
+// Database models exports
+export { default as Event } from './event.model';
+export { default as Booking } from './booking.model';
+
+// TypeScript interfaces exports
+export type { IEvent } from './event.model';
+export type { IBooking } from './booking.model';
