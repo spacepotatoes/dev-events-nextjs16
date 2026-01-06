fa54bf20a6ab4c617c19d56e22ccba07289aeea1
spacepotatoes <gtroiano77@gmx.de>
2026-01-06 18:59:57 +0000

    vercel env

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import ExploreBtn from "@/components/ExploreBtn"
 import EventCard from "@/components/EventCard"
-import events from "@/lib/constants"
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
 
